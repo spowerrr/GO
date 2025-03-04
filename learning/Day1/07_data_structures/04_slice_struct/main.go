5fcb5d67840a1f0773bb9cc5af8a2795daa47c8a
spowerrr <sk1969363@gmail.com>
2025-03-05 00:39:33 +0600

    added 04_slice_struct

--- learning/Day1/07_data_structures/04_slice_struct/main.go
+++ learning/Day1/07_data_structures/04_slice_struct/main.go
@@ -1,41 +1,51 @@
 package main
 
 import (
+	"log"
+	"os"
 	"text/template"
 )
+
+var tpl *template.Template
 
 type sage struct {
 	Name  string
 	Motto string
 }
 
-var tpl *template.Template
-
 func init() {
 	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
 }
-func main(){
-  Buddha :=sage{
-    Name: "Buddha",
-    Motto: "The belief of no beliefs",
-  }
-  Gandhi :=sage{
-    Name: "Gandhi",
-    Motto: "The belief of no beliefs",
-  }
-  Mlk :=sage{
-    Name: "Mlk",
-    Motto: "The belief of no beliefs",
-  }
-  Jesus :=sage{
-    Name: "Jesus",
-    Motto: "The belief of no beliefs",
-  }Muhammad :=sage{
-    Name: "Muhammad",
-    Motto: "The belief of no beliefs",
-  }Buddha :=sage{
-    Name: "Buddha",
-    Motto: "The belief of no beliefs",
-  }
-  
+
+func main() {
+	buddha := sage{
+		Name:  "Buddha",
+		Motto: "The belief of no beliefs",
+	}
+
+	gandhi := sage{
+		Name:  "Gandhi",
+		Motto: "Be the change",
+	}
+
+	mlk := sage{
+		Name:  "Martin Luther King",
+		Motto: "Hatred never ceases with hatred but with love alone is healed.",
+	}
+
+	jesus := sage{
+		Name:  "Jesus",
+		Motto: "Love all",
+	}
+
+	muhammad := sage{
+		Name:  "Muhammad",
+		Motto: "To overcome evil with good is good, to resist evil by evil is evil.",
+	}
+
+	sages := []sage{buddha, gandhi, mlk, jesus, muhammad}
+	err := tpl.Execute(os.Stdout, sages)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
